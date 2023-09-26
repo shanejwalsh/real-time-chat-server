6ae0fa1ea121e4823e16b6c42aebd6e4f687904c
Shane Walsh <shanewalsh@hey.com>
2023-09-26 19:21:47 +0100

    initial commit

---
+++ prisma/migrations/20230926180603_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "author" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
